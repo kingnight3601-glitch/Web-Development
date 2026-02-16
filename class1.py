dbfaa86de813f97cf6a57067b4a92d5e6d142844
Fatima waqas <kingnight3601@gmail.com>
2026-02-16 20:22:34 +0500

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,47 @@
+name="Fatima"
+age=15
+do_you_like_python=True
+your_kilowehight=39.5
+
+
+
+print(name)
+print(age)
+print(do_you_like_python)
+print(your_kilowehight)
+print(str(age))
+print(int(your_kilowehight))
+print(float(age))
+
+
+
+number1=10
+number2=15
+
+print(number1)
+print(number2)
+print(number1+number2)
+print(number1-number2)
+print(number1*number2)
+print(number1/number2)
+print(number1%number2)
+print(number1//number2)
+print(number1**2)
+print(number2**3)
+
+
+print("equal=", number1==number2)
+print("not equal=",number1!=number2)
+print("greater than=",number1>number2)
+print("less than=",number1<number2)
+print("greater than or equals to=",number1>=number2)
+print("less than or equals to=",number1<=number2)
+
+
+result=number1-number2//2
+print(result)
+
+
+
+
+
