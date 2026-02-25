03eca5a2ec1eaaeebc2fa42a188684ce2a5e78e4
Fatima waqas <kingnight3601@gmail.com>
2026-02-25 20:22:41 +0500

    Add files via upload

---
+++ attendence.py
@@ -0,0 +1,21 @@
+attendence =int(input("Enter your monthly attendence:"))
+if attendence <=90:
+    print("you are eligible for promotion")
+elif attendence <=80:
+    print("you are eligible but improve your attendence")
+elif attendence <=70:
+    print("your attendence is short but you are hardly promoted")
+else:
+    print("you are not eligible for promotion")
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ loop.py
@@ -0,0 +1,19 @@
+num=23
+for i in range(1,11):
+    print("23 X",i," = ",num*i)
+var="Fatima"
+for i in range(1,100):
+    print(var)
+
+for i in range(1,101):
+    print(i)   
+
+n=int(input("Enter a number:"))
+for i in range (1,n+1):
+    for j in range(i):
+        print('*',end='')
+    print()         
+total_sum=sum(range(1,11))
+print(f"the sum of the first 10 natural numbers is {total_sum}")
+
+
