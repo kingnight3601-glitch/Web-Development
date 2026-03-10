23660dbc6462c4081a97f72f943d4754981eaae8
Fatima waqas <kingnight3601@gmail.com>
2026-03-10 15:37:34 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+def greet():
+    print("hello welcome to my page")
+greet()
+
+def intro(name):
+    print("my name is ",name)
+intro("sidra")
+
+def intro(name):
+
+     print("good morning ",name)
+name=input("enter your name:")
+intro(name)
+
+def add(x,y):
+    return x+y
+def sub(x,y):
+    return x-y
+def mul(x,y):
+    return x*y
+def div(x,y):
+    return x/y
+num1=int(input("enter first number:"))
+num2=int(input("enter second number:"))
+print("the sum is:",add(num1,num2))
+print("the difference is:",sub(num1,num2))
+print("the product is:",mul(num1,num2))
+print("the quotient is:",div(num1,num2))
+
+    
