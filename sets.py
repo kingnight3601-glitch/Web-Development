8774cc7d236bd20a72144b0868f834efc2ebe3ce
Fatima waqas <kingnight3601@gmail.com>
2026-03-19 16:32:17 +0500

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,21 @@
+my_set ={1,2,3,3,4,5,5}
+print(my_set)
+my_set.add(6)
+print(my_set)
+my_set.remove(3)
+print(my_set)   
+set1=my_set
+set2={4,5,6,7,8}
+print(set1)
+print(set2)
+print("difference")
+print(set1.difference(set2))
+print("semmetry difference")
+print(set1.symmetric_difference(set2))
+set1={"pink","blue","green"}
+set2={"blue","yellow","red"}
+print("union")
+print(set1.union(set2))
+print("intersection")
+print(set1.intersection(set2))
+
