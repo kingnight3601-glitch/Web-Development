6a001540e637a82bf1f70096d33cf6290014fb05
Fatima waqas <kingnight3601@gmail.com>
2026-03-19 15:56:49 +0500

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,25 @@
+def fiboccino(num):
+ num=int(input("enter any number:"))
+ fiboccino(num)
+if num<=0:
+        print("there is an error")
+        
+        
+elif int==1:
+        print("the fibonacci sequence is:",0)
+elif int==2:
+        print("the fibonacci sequence is:",0,1)
+else:
+        n1,n2=0,1
+        sum=0
+        print("the fibonacci sequence is :",end="")
+        for i in range(0,int):
+            print(sum,end="")
+            n1=n2
+            n2=sum
+            sum=n1+n2
+            
+
+
+
+
