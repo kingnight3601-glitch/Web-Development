6b71225447f28193103685ce79bbd1e98b70e137
Fatima waqas <kingnight3601@gmail.com>
2026-03-25 19:40:53 +0500

    Add files via upload

---
+++ numberguessing.py
@@ -0,0 +1,13 @@
+import random
+number=random.randint(1,100)
+attempts=0
+while True:
+    guess=int(input("Guess a number between 1 and 100: "))
+    attempts+=1
+    if guess < number:
+        print("Too low! Try again.")
+    elif guess > number:
+        print("Too high! Try again.")
+    else:
+        print(f"Congratulations! You've guessed the number {number} in {attempts} attempts.")
+        break   
---
+++ tupletolist.py
@@ -0,0 +1,4 @@
+my_tuple=(1,2,3,4,5)
+print(my_tuple)
+my_list=list(my_tuple)
+print(my_list)
